0e804d69b33b3f03032582f9a1ecd44326498509
Dhruvesh <dhruvesh.baria@actonate.com>
2023-05-28 17:16:43 +0530

    khadas led

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::os::unix::net::UnixStream;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let path = "/sys/class/leds/sys_led/trigger";
+    let mut stream = UnixStream::connect(path)?;
+    stream.write_all(b"none")?;
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+    println!("{response}");
+    Ok(())
+}
