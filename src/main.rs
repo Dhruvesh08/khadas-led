de336f31cc405f3fd7ddfd0786e97e81ac843c37
Dhruvesh <dhruvesh.baria@actonate.com>
2023-05-28 18:07:38 +0530

    khadas led off

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::fs::write;
+// use std::fs::write;
 
-fn main() -> std::io::Result<()> {
-    write("/sys/class/leds/sys_led/trigger", "none")?;
-    Ok(())
+// fn main() -> std::io::Result<()> {
+//     write("/sys/class/leds/sys_led/trigger", "none")?;
+//     Ok(())
+// }
+
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let mut file = match File::open("/sys/class/leds/sys_led/trigger") {
+        Ok(file) => file,
+        Err(e) => panic!("Error opening file: {}", e),
+    };
+    file.write_all(b"none").unwrap();
 }
