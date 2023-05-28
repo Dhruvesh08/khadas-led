c3f968d70d810a1338c2262f97bdc607796dfd1f
Dhruvesh <dhruvesh.baria@actonate.com>
2023-05-28 20:34:57 +0530

    khadas led all configs added

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,22 @@ use std::fs::{File};
 use std::io::{self, prelude::*};
 
 fn main() -> io::Result<()> {
-    let mut file = File::create("/sys/class/leds/sys_led/trigger")?;
+    let  file = File::create("/sys/class/leds/sys_led/trigger")?;
+    turn_led_on(file)?;
+    Ok(())
+}
+
+
+pub fn turn_led_on(mut file:File) -> io::Result<()> {
+    file.write(b"default-on")?;
+    Ok(())
+}
+
+pub fn turn_led_off(mut file:File) -> io::Result<()> {  
     file.write(b"none")?;
+    Ok(())
+}
+pub fn turn_led_blink(mut file:File) -> io::Result<()> {  
+    file.write(b"heartbeat")?;
     Ok(())
 }
