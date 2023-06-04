8fc63ed06c7f48eb54d2d7ed275db66258095dee
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-04 19:03:56 +0530

    khadas led controls

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
-// use std::fs::write;
+use std::io::{self, Write};
+use std::process::Command;
 
-// fn main() -> std::io::Result<()> {
-//     write("/sys/class/leds/sys_led/trigger", "none")?;
-//     Ok(())
-// }
+fn execute_command(command: &str) -> Result<(), std::io::Error> {
+    let output = Command::new("/bin/sh")
+        .arg("-c")
+        .arg(command)
+        .output()?;
 
-use std::fs::{File};
-use std::io::{self, prelude::*};
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!(
+                "Command '{}' failed with exit code {:?}",
+                command, output.status.code()
+            ),
+        ))
+    }
+}
 
-fn main() -> io::Result<()> {
-    let mut file = File::create("/sys/class/leds/sys_led/trigger")?;
-    file.write(b"none")?;
+fn main() -> Result<(), std::io::Error> {
+    let mut input = String::new();
+
+    print!("Enter a value for LED control: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut input)?;
+
+    match input.trim() {
+        "none" => execute_command("echo none | sudo tee /sys/class/leds/sys_led/trigger")?,
+        "default-on" => execute_command("echo default-on | sudo tee /sys/class/leds/sys_led/trigger")?,
+        "heartbeat" => execute_command("echo heartbeat | sudo tee /sys/class/leds/sys_led/trigger")?,
+        _ => println!("Invalid input!"),
+    }
+
     Ok(())
 }
